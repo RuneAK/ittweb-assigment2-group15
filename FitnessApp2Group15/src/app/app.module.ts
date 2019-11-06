aa1912393fbf06bf72fa6f53aa03af5ec6ead15d
Rune <runekeena@gmail.com>
2019-11-06 17:05:23 +0100

    material design

--- FitnessApp2Group15/src/app/app.module.ts
+++ FitnessApp2Group15/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { RegisterComponent } from './components/user/register/register.component
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/user/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
